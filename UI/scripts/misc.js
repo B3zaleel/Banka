9f5efeaff66ac156aec86586b62c983028ce0d46
Bezaleel Olakunori <bezaleeloci@gmail.com>
2019-04-09 17:33:30 +0100

    Fixed the padding of the form panels.

---
+++ UI/scripts/misc.js
@@ -0,0 +1,6 @@
+
+/**
+ * Gets the short version of a DateTime object.
+ * @param {*} dateObj
+ */
+const getShortDate = (dateObj) => `${dateObj.getDay()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`;
